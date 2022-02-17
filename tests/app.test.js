bd3e1604790bfc19b15d26a9dd98dd1477c17edf
Simii Vasaikar <vasaikar.s@northeastern.edu>
2022-02-17 16:00:41 -0500

    modified test

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require('./app')
+const app = require('../app')
 
 describe("Get Test example", () => {
     test('successful GET api call', async() => {
