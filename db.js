b568eb6a37e273f95b6a63f37ae17a2d0d099f90
Simii Vasaikar <71291381+vasaikarSimii@users.noreply.github.com>
2022-03-11 00:07:37 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool
 
 const pool = new Pool({
-    user: "postgres",
-    password:"Coco@786",
-    host: "localhost",
-    port: 5432,
-    database: "cloud_database"
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOSTNAME,
+    port: process.env.PORT,
+    database: process.env.DB_NAME
 })
 
 module.exports = pool;
