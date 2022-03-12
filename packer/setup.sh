857044ff1d20f2b7a163118a2c2550f20caf5a76
Simii Vasaikar <71291381+vasaikarSimii@users.noreply.github.com>
2022-03-12 14:11:44 -0500

    Update setup.sh

--- packer/setup.sh
+++ packer/setup.sh
@@ -5,7 +5,9 @@ sudo yum update -y
 sudo yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
 sudo yum install -y nodejs
-sudo yum install -y postgresql
+chown ec2-user:ec2-user /tmp/pgdg.repo
+sudo mv /tmp/pgdg.repo /etc/yum.repos.d/pgdg.repo
+sudo yum install -y postgresql12
 
 cd /tmp
 sudo unzip webservice.zip
