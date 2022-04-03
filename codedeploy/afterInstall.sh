cb41e8580b1dfc4a78e627516e81d13f3948adda
Seemin Vasaikar <seeminvasaikar@Seemins-MacBook-Pro.local>
2022-04-02 21:08:02 -0400

    upd

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pm2 kill
 cd /tmp
-sudo unzip webservice.zip
+# sudo unzip webservice.zip
 # sudo mv /tmp/webservice/packer/nodeapp.service /etc/systemd/system/nodeapp.service
 sudo chown -R ec2-user:ec2-user webservice
 cd webservice
